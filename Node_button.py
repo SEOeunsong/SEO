20c0626107608974b73b8caa1d5ae4e541e45d2c
SEOeunsong <ekwjd6307@naver.com>
2017-07-28 16:20:44 +0900

    button_gui_py

---
+++ Node_button.py
@@ -0,0 +1,92 @@
+'''
+author : seo eun song, kim kang min
+email  : ekwjd6307@naver.com ,kangmin_kim@mofac-alfred.com
+'''
+
+import sys, collections
+from PySide.QtCore import *
+from PySide.QtGui import *
+import nukescripts
+import button_gui
+reload(button_gui)
+
+class Seo(QDialog, button_gui.Ui_Dialog):
+    def __init__(self, parent=QApplication.desktop()):
+        super(Seo, self).__init__(parent)
+        self.setupUi(self)
+        
+        self.readnode_listView.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.list_setting.setSelectionMode(QAbstractItemView.ExtendedSelection)
+
+        readList = []
+        listList = ['RotoPaint','FrameHold','Transform','Tracker','Reformat','ColorCorrect','HueCorrect']
+
+        allReads = nuke.allNodes('Read')
+        for read in allReads:
+            readList.append(read['name'].getValue())
+        readList.sort(key=lambda x: int(x[4:]))
+
+        model  = QStandardItemModel(self.readnode_listView)
+        for r in readList:
+            item = QStandardItem(r)
+            model.appendRow(item)
+
+        mod  = QStandardItemModel(self.list_setting)
+        for l in listList:
+            set = QStandardItem(l)
+            mod.appendRow(set)
+        self.list_setting.setModel(mod)          
+
+        self.readnode_listView.setModel(model)     
+        self.exec_button.clicked.connect(self.createSettingNode)
+        self.del_button.clicked.connect(self.deleteSettingNode)
+
+
+    def createSettingNode(self):
+        nukescripts.clear_selection_recursive()
+        read_node = []
+        node_created = []
+        create_dic = {}
+        self.extendList = []
+
+        selectedReadNode = self.readnode_listView.selectedIndexes()
+        for readnode in selectedReadNode:
+            nodename = readnode.data()
+            read_node.append(nodename)
+
+        selectedSettingNode = self.list_setting.selectedIndexes()
+        for snode in selectedSettingNode:      
+        # FrameHold, RotoPaint 를 0FrameHold, 3RotoPaint 와 같이 스트링을 만들어 node_created 리스트에 append     
+            node_created.append("{0}_{1}".format(snode.row(), snode.data()))
+       
+        # node_created  비교 후 정렬
+        node_created.sort(key=lambda x: int(x[0:1]))   
+
+        # create_dic를 orderedDict 형태로 바꾼뒤 키, 밸류 값을 집어넣을것     
+        for num, data in enumerate(read_node, 0):    
+            create_dic[data] = node_created
+            create_dic = collections.OrderedDict(sorted(create_dic.items(), key=lambda x: x[0]))
+
+        for key, value in create_dic.items():
+            readNode = nuke.toNode(key)
+
+            createdNode = [eval('nuke.nodes.%s()' %x[2:]) for x in create_dic[key]] 
+            self.extendList.extend(createdNode)
+
+            for num, data in enumerate(createdNode, 0):
+                if num == 0:
+                    createdNode[num].setInput(0, readNode)
+                else:
+                    createdNode[num].setInput(0, createdNode[num-1])
+   
+
+    def deleteSettingNode(self):
+        for deleteNode in self.extendList:
+            nuke.delete(deleteNode)
+
+                    
+def main():
+    window = Seo()
+    window.show()
+
+main()
--- button_gui.py
+++ button_gui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'button20170717.ui'
 #
-# Created: Wed Jul 19 17:19:44 2017
+# Created: Fri Jul 28 16:14:46 2017
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,28 +12,45 @@ from PySide import QtCore, QtGui
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(539, 602)
+        Dialog.resize(311, 287)
         self.read_gb = QtGui.QGroupBox(Dialog)
-        self.read_gb.setGeometry(QtCore.QRect(10, 20, 141, 231))
+        self.read_gb.setGeometry(QtCore.QRect(10, 20, 131, 231))
         self.read_gb.setObjectName("read_gb")
         self.readnode_listView = QtGui.QListView(self.read_gb)
-        self.readnode_listView.setGeometry(QtCore.QRect(10, 20, 121, 192))
+        self.readnode_listView.setGeometry(QtCore.QRect(10, 20, 111, 192))
         self.readnode_listView.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
         self.readnode_listView.setSelectionMode(QtGui.QAbstractItemView.MultiSelection)
         self.readnode_listView.setFlow(QtGui.QListView.TopToBottom)
         self.readnode_listView.setObjectName("readnode_listView")
         self.setting_nd = QtGui.QGroupBox(Dialog)
-        self.setting_nd.setGeometry(QtCore.QRect(170, 20, 141, 231))
+        self.setting_nd.setGeometry(QtCore.QRect(170, 20, 131, 231))
         self.setting_nd.setObjectName("setting_nd")
         self.list_setting = QtGui.QListView(self.setting_nd)
-        self.list_setting.setGeometry(QtCore.QRect(10, 20, 121, 192))
+        self.list_setting.setGeometry(QtCore.QRect(10, 20, 111, 192))
         self.list_setting.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
         self.list_setting.setSelectionMode(QtGui.QAbstractItemView.MultiSelection)
         self.list_setting.setFlow(QtGui.QListView.TopToBottom)
         self.list_setting.setObjectName("list_setting")
+        self.up_btn = QtGui.QPushButton(self.setting_nd)
+        self.up_btn.setGeometry(QtCore.QRect(30, 200, 31, 23))
+        self.up_btn.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/icons/up_arrow.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.up_btn.setIcon(icon)
+        self.up_btn.setObjectName("up_btn")
+        self.down_btn = QtGui.QPushButton(self.setting_nd)
+        self.down_btn.setGeometry(QtCore.QRect(70, 200, 31, 23))
+        self.down_btn.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/icons/down_arrow.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.down_btn.setIcon(icon1)
+        self.down_btn.setObjectName("down_btn")
         self.exec_button = QtGui.QPushButton(Dialog)
-        self.exec_button.setGeometry(QtCore.QRect(320, 190, 71, 23))
+        self.exec_button.setGeometry(QtCore.QRect(80, 260, 71, 23))
         self.exec_button.setObjectName("exec_button")
+        self.del_button = QtGui.QPushButton(Dialog)
+        self.del_button.setGeometry(QtCore.QRect(160, 260, 71, 23))
+        self.del_button.setObjectName("del_button")
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -42,5 +59,6 @@ class Ui_Dialog(object):
         Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
         self.read_gb.setTitle(QtGui.QApplication.translate("Dialog", "Read Nodes", None, QtGui.QApplication.UnicodeUTF8))
         self.setting_nd.setTitle(QtGui.QApplication.translate("Dialog", "Setting Nodes", None, QtGui.QApplication.UnicodeUTF8))
-        self.exec_button.setText(QtGui.QApplication.translate("Dialog", "execute", None, QtGui.QApplication.UnicodeUTF8))
+        self.exec_button.setText(QtGui.QApplication.translate("Dialog", "create", None, QtGui.QApplication.UnicodeUTF8))
+        self.del_button.setText(QtGui.QApplication.translate("Dialog", "delete", None, QtGui.QApplication.UnicodeUTF8))
 
